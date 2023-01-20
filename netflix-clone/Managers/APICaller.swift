51501394550edca394197e60c74341a9e628cce6
irfanizudin <irfanizzudyn@gmail.com>
2023-01-20 23:04:21 +0700

    setup: add .gitignore

---
+++ netflix-clone/Managers/APICaller.swift
@@ -0,0 +1,13 @@
+//
+//  APICaller.swift
+//  netflix-clone
+//
+//  Created by Irfan Izudin on 20/01/23.
+//
+
+import Foundation
+
+
+struct Constants {
+    static let API_KEY = APIKey.tmdbAPIKey
+}
