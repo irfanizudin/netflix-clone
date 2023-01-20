4b8d52e6e91c5d330bb161c374ca83ab74211bb4
irfanizudin <irfanizzudyn@gmail.com>
2023-01-20 10:13:47 +0700

    Initial Commit

---
+++ netflix-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflix-clone
+//
+//  Created by Irfan Izudin on 20/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
