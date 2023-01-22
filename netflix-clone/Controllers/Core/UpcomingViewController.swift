de058275d701414ad6a1a2d20da2fdc80975f1c4
irfanizudin <irfanizzudyn@gmail.com>
2023-01-22 10:14:39 +0700

    feat: create upcoming page

--- netflix-clone/Controllers/Core/UpcomingViewController.swift
+++ netflix-clone/Controllers/Core/UpcomingViewController.swift
@@ -9,11 +9,65 @@ import UIKit
 
 class UpcomingViewController: UIViewController {
 
+    private var movies: [Movie] = []
+    
+    private let upcomingTableVIew: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UpcomingTableViewCell.self, forCellReuseIdentifier: UpcomingTableViewCell.identifier)
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
-
+        title = "Upcoming"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        view.addSubview(upcomingTableVIew)
+        upcomingTableVIew.delegate = self
+        upcomingTableVIew.dataSource = self
+        
+        fetchUpcomingMovies()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTableVIew.frame = view.bounds
+    }
+    
+    private func fetchUpcomingMovies() {
+        APICaller.shared.getUpcomingMovies { [weak self] result in
+            switch result {
+            case.success(let movies):
+                self?.movies = movies
+                DispatchQueue.main.async {
+                    self?.upcomingTableVIew.reloadData()
+                }
+            case.failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 
+}
+
+extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: UpcomingTableViewCell.identifier, for: indexPath) as? UpcomingTableViewCell else {
+            return UITableViewCell()
+        }
+        let movie = movies[indexPath.row]
+        cell.configureUpcomingMovie(movies: movie)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
 }
---
+++ netflix-clone/Views/UpcomingTableViewCell.swift
@@ -0,0 +1,78 @@
+//
+//  UpcomingTableViewCell.swift
+//  netflix-clone
+//
+//  Created by Irfan Izudin on 21/01/23.
+//
+
+import UIKit
+import SDWebImage
+
+class UpcomingTableViewCell: UITableViewCell {
+
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "heroImage")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .headline)
+        label.textColor = .white
+        label.text = "Avatar"
+        label.numberOfLines = 2
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let playButtonCircle: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "play.circle")?.withTintColor(.white, renderingMode: .alwaysOriginal)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    static let identifier = "UpcomingTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(posterImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(playButtonCircle)
+        applyConstraints()
+    }
+    
+    private func applyConstraints() {
+        NSLayoutConstraint.activate([
+            posterImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            posterImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+            posterImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5),
+            posterImageView.widthAnchor.constraint(equalToConstant: 100),
+            
+            titleLabel.leadingAnchor.constraint(equalTo: posterImageView.trailingAnchor, constant: 10),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: playButtonCircle.leadingAnchor, constant: -10),
+            
+            playButtonCircle.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            playButtonCircle.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            playButtonCircle.widthAnchor.constraint(equalToConstant: 40),
+            playButtonCircle.heightAnchor.constraint(equalToConstant: 40),
+        ])
+    }
+    
+    public func configureUpcomingMovie(movies: Movie) {
+        guard let url = URL(string: "https://image.tmdb.org/t/p/w500\(movies.poster_path ?? "")") else { return }
+        posterImageView.sd_setImage(with: url)
+        titleLabel.text = movies.title ?? movies.original_title
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
